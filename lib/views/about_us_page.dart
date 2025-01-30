984a0665fbfcfffa0cee7bfe70ee564e0db07b0b
ChrisKorte55 <151131289+ChrisKorte55@users.noreply.github.com>
2025-01-30 17:38:56 +0100

    IFrame for About Us page working

--- lib/views/about_us_page.dart
+++ lib/views/about_us_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class AboutUsPage extends StatelessWidget {
   const AboutUsPage({Key? key}) : super(key: key);
@@ -7,11 +8,19 @@ class AboutUsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("About Us"),
+        title: const Text("About Us"),
         backgroundColor: Colors.white,
       ),
       body: Center(
-        child: Text("Welcome to the About Us page!"),
+        child: HtmlWidget(
+          '''
+            <iframe src="https://kloster-einsiedeln.ch/" 
+            width="${MediaQuery.of(context).size.width}"
+            height="${MediaQuery.of(context).size.height - AppBar().preferredSize.height - MediaQuery.of(context).padding.top}"
+            frameborder="0" 
+            allowfullscreen></iframe>
+          ''',
+        ),
       ),
     );
   }
