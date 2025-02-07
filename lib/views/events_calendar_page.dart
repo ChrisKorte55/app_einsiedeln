554a6f6a7a96e35a6795c1bf9a160ba5c1186dfd
ChrisKorte55 <151131289+ChrisKorte55@users.noreply.github.com>
2025-02-07 13:04:55 +0100

    Event Calendar and Live-stream working. Tour still needs restructuring

--- lib/views/events_calendar_page.dart
+++ lib/views/events_calendar_page.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class EventsPage extends StatelessWidget {
   const EventsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Events"),
+        title: const Text("Events"),
         backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
       ),
-      body: Center(
-        child: Text("Welcome to the Events page!"),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "Upcoming Events Schedule",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: HtmlWidget(
+                '''
+                  <iframe 
+                    src="https://www.kloster-einsiedeln.ch/gettimes_2024_08/" 
+                    width="$screenWidth"
+                    height="${screenHeight * 0.8}"
+                    frameborder="0" 
+                    allow="fullscreen"
+                    style="border-radius: 10px; border: 1px solid #ccc;"
+                  ></iframe>
+                ''',
+                key: UniqueKey(), // Ensures the iframe reloads correctly
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
