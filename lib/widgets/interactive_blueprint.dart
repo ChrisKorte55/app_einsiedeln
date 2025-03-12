d0a963a5d38defe94b5d69ef0a9b89f36f8e00e0
ChrisKorte55 <151131289+ChrisKorte55@users.noreply.github.com>
2025-03-12 12:59:53 +0100

    icons in the right places for the interactive blueprint

--- lib/widgets/interactive_blueprint.dart
+++ lib/widgets/interactive_blueprint.dart
@@ -5,81 +5,128 @@ import 'package:app_einsiedeln/widgets/location_details_popup.dart';
 class InteractiveBlueprint extends StatelessWidget {
   final List<TourLocation> locations;
 
-  const InteractiveBlueprint({Key? key, required this.locations}) : super(key: key);
+  InteractiveBlueprint({Key? key, required this.locations}) : super(key: key);
 
   final double originalImageWidth = 442.0;
   final double originalImageHeight = 734.0;
+  final Color primaryColor = Color.fromRGBO(176, 148, 60, 1);
+  final Color redColor = Colors.red;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: InteractiveViewer(
-        panEnabled: true,
-        scaleEnabled: true,
-        minScale: 0.5,
-        maxScale: 2.5,
-        child: AspectRatio(
-          aspectRatio: originalImageWidth / originalImageHeight,
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              double displayWidth = constraints.maxWidth;
-              double displayHeight = constraints.maxHeight;
+    return Stack(
+      children: [
+        Center(
+          child: InteractiveViewer(
+            panEnabled: true,
+            scaleEnabled: true,
+            minScale: 0.5,
+            maxScale: 2.5,
+            child: AspectRatio(
+              aspectRatio: originalImageWidth / originalImageHeight,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  double displayWidth = constraints.maxWidth;
+                  double displayHeight = constraints.maxHeight;
 
-              return Stack(
-                children: [
-                  Image.asset('assets/images/floorplanzoomedin.png', width: displayWidth, height: displayHeight, fit: BoxFit.contain, ),
-                  Positioned(
-                    left: 10,
-                    top: 10,
-                    child: GestureDetector(
-                      onTap: () => _showMiniMap(context),
-                      child: Container(
-                        width: 140,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.red, width: 2),
-                          boxShadow: [BoxShadow(color: Color(0x4D000000), spreadRadius: 2, blurRadius: 2, offset: Offset(0, 2),),],
-                          image: DecorationImage(image: AssetImage('assets/images/kloster_uebersicht.jpg'), fit: BoxFit.cover,),
-                        ),
+                  return Stack(
+                    children: [
+                      Image.asset(
+                        'assets/images/floorplanzoomedin.png',
+                        width: displayWidth,
+                        height: displayHeight,
+                        fit: BoxFit.contain,
                       ),
-                    ),
-                  ),
-                  ...locations.map((location) {
-                    double xPosition = (location.x / originalImageWidth) * displayWidth - 15;
-                    double yPosition = (location.y / originalImageHeight) * displayHeight - 15;
+                      ...locations.map((location) {
+                        double xPosition =
+                            (location.x / originalImageWidth) * displayWidth;
+                        double yPosition =
+                            (location.y / originalImageHeight) * displayHeight;
 
-                    return Positioned(
-                      left: xPosition,
-                      top: yPosition,
-                      child: GestureDetector(
-                        onTap: () => showLocationDetails(context, location),
-                        child: Column(
+                        return Stack(
                           children: [
-                            Container(
-                              width: 30,
-                              height: 30,
-                              decoration: BoxDecoration(color: Colors.red, shape: BoxShape.circle,
-                                boxShadow: [BoxShadow(color: Color(0x4D000000), spreadRadius: 1, blurRadius: 2, offset: Offset(0, 2),),],
-                              ),
-                              alignment: Alignment.center,
-                              child: Text(
-                                location.id.toString(),
-                                style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 10,),
+                            Positioned(
+                              left: xPosition - 15,
+                              top: yPosition - 15,
+                              child: GestureDetector(
+                                onTap: () => showLocationDetails(context, location),
+                                child: AnimatedContainer(
+                                  duration: Duration(milliseconds: 200),
+                                  width: 30,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    color: redColor,
+                                    shape: BoxShape.circle,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black54,
+                                        spreadRadius: 1,
+                                        blurRadius: 6,
+                                        offset: Offset(-1, -1),
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.white38,
+                                        spreadRadius: 1,
+                                        blurRadius: 3,
+                                        offset: Offset(2, 2),
+                                      ),
+                                    ],
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    location.id.toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                            SizedBox(height: 4),
-                            Text(location.name, style: TextStyle(color: Colors.black, fontSize: 12), textAlign: TextAlign.center, maxLines: 2, overflow: TextOverflow.ellipsis,),
+                            Positioned(
+                              left: xPosition - 40,
+                              top: yPosition + 20,
+                              child: SizedBox(
+                                width: 80,
+                                child: Text(
+                                  location.name,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w700,
+                                    height: 1.2,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ),
                           ],
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ],
-              );
-            },
+                        );
+                      }).toList(),
+                    ],
+                  );
+                },
+              ),
+            ),
           ),
         ),
-      ),
+        Positioned(
+          right: 16,
+          bottom: 16,
+          child: FloatingActionButton(
+            onPressed: () => _showMiniMap(context),
+            backgroundColor: redColor,
+            child: Icon(Icons.map, color: Colors.white, size: 30),
+            shape: CircleBorder(),
+            elevation: 8,
+            splashColor: Colors.white,
+            highlightElevation: 12,
+          ),
+        ),
+      ],
     );
   }
 
@@ -88,9 +135,19 @@ class InteractiveBlueprint extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          content: Image.asset('assets/images/kloster_uebersicht.jpg', fit: BoxFit.cover),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          content: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset('assets/images/kloster_uebersicht.jpg', fit: BoxFit.cover),
+          ),
           actions: <Widget>[
-            TextButton(onPressed: () => Navigator.of(context).pop(), child: Text('Close'),),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text(
+                'Close',
+                style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
         );
       },
