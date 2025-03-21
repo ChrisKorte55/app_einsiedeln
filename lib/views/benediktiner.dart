1347a11229b5f37d9d748c9899a8421790c7e8ec
ChrisKorte55 <151131289+ChrisKorte55@users.noreply.github.com>
2025-03-21 12:57:21 +0100

    Benediktiner page improvements

--- lib/views/benediktiner.dart
+++ lib/views/benediktiner.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 class Benediktiner extends StatelessWidget {
   final Color primaryColor = Color.fromRGBO(176, 148, 60, 1);
@@ -15,122 +14,57 @@ class Benediktiner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appLoc = AppLocalizations.of(context)!;
-    final textStyle = TextStyle(fontSize: 18.0, height: 1.5, color: Colors.grey[800]);
+    final textStyle = TextStyle(fontSize: 18.0, height: 1.5, color: Colors.black);
     final titleStyle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: primaryColor);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(appLoc.benediktiner, style: titleStyle),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
         iconTheme: IconThemeData(color: Colors.black),
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    appLoc.benediktinerIntro,
-                    style: textStyle,
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              CarouselSlider(
-                options: CarouselOptions(
-                  aspectRatio: 16/9,
-                  enlargeCenterPage: true,
-                  scrollDirection: Axis.horizontal,
-                  autoPlay: true,
-                ),
-                items: images.map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                          child: Image.asset(i, fit: BoxFit.cover),
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
-              SizedBox(height: 20),
-              Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    appLoc.benediktinerPar1,
-                    style: textStyle,
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    appLoc.benediktinerPar2,
-                    style: textStyle,
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    appLoc.benediktinerPar3,
-                    style: textStyle,
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    appLoc.benediktinerPar4,
-                    style: textStyle,
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ),
-            ],
-          ),
+      body: Container(
+        color: Colors.white, // Set the background to white
+        child: ListView(
+          children: <Widget>[
+            textCard(appLoc.benediktinerIntro, textStyle),
+            imageCard(images[0]),
+            textCard(appLoc.benediktinerPar1, textStyle),
+            imageCard(images[1]),
+            textCard(appLoc.benediktinerPar2, textStyle),
+            imageCard(images[2]),
+            textCard(appLoc.benediktinerPar3, textStyle),
+            imageCard(images[3]),
+            textCard(appLoc.benediktinerPar4, textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textCard(String text, TextStyle style) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(16),
+      color: Colors.white, // Ensure text cards are also white
+      child: Text(
+        text,
+        style: style,
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
+  Widget imageCard(String imagePath) {
+    return Container(
+      width: double.infinity,
+      child: Card(
+        color: Colors.white, // Card with white background
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 0),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
         ),
       ),
     );
